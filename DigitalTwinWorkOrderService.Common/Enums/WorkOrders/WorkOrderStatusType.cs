6edc22b9ca63061a35f14b615502a62ee95ea9af
Deon <deonlpretorius@outlook.com>
2026-09-03 12:04:29 +0200

    Added in the Work Order Status Type under enums.

---
+++ DigitalTwinWorkOrderService.Common/Enums/WorkOrders/WorkOrderStatusType.cs
@@ -0,0 +1,16 @@
+﻿/// <summary>
+/// Namespace <c>DigitalTwinWorkOrderService.Enums.WorkOrders</c> contains the enumerations for the application.
+/// </summary>
+namespace DigitalTwinWorkOrderService.Enums.WorkOrders
+{
+    /// <summary>
+    /// Enum <c>WorkOrderStatusType</c> represents the status of a work order.
+    /// </summary>
+    public enum WorkOrderStatusType : int
+    {
+        Pending = 1,
+        Accepted = 2,
+        Completed = 3,
+        Cancelled = 4
+    }
+}
