ce86abf58f68cd8fd25e56621b8626a7fd9eeb33
Deon <deonlpretorius@outlook.com>
2026-09-03 13:37:44 +0200

    Added in the Work Order Processor service project and files.

---
+++ DigitalTwinWorkOrderService.WorkOrderProcessor/Data/WorkOrderProcessorDbContext.cs
@@ -0,0 +1,32 @@
+﻿using DigitalTwinWorkOrderService.Models.WorkOrders;
+using Microsoft.EntityFrameworkCore;
+
+/// <summary>
+/// Namespace <c>DigitalTwinWorkOrderService.WorkOrderProcessor.Data</c> contains the database access layer operations for the application.
+/// </summary>
+namespace DigitalTwinWorkOrderService.WorkOrderProcessor.Data
+{
+    /// <summary>
+    /// Class <c>WorkOrderProcessorDbContext</c> represents the data access layer context for the Work Order Processor.
+    /// <remarks>
+    /// Inherits from DbContext <see cref="DbContext"/>
+    /// </remarks>
+    /// </summary>
+    public class WorkOrderProcessorDbContext : DbContext
+    {
+        public WorkOrderProcessorDbContext(DbContextOptions<WorkOrderProcessorDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<WorkOrderEvent> WorkOrderEvents { get; set; }
+
+        public DbSet<WorkOrder> WorkOrders { get; set; }
+
+        public DbSet<WorkOrderHistory> WorkOrderHistories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ DigitalTwinWorkOrderService.WorkOrderProcessor/Interfaces/IQueueService.cs
@@ -0,0 +1,16 @@
+﻿using DigitalTwinWorkOrderService.Models.WorkOrders;
+
+/// <summary>
+/// Namespace <c>DigitalTwinWorkOrderProcessor.Interfaces</c> contains the contract for managing background tasks.
+/// </summary>
+namespace DigitalTwinWorkOrderProcessor.Interfaces
+{
+    /// <summary>
+    /// Interface <c>IQueueService</c> represents the contract for the Queue Service.
+    /// </summary>
+    public interface IQueueService<T>
+    {
+        void Enqueue(T workOrderEvent);
+        bool TryDequeue(out T workOrderEvent);
+    }
+}
